bf2d2638636de07c86f3f89a6fc42a13ccd35d16
preke <geek00021@gmail.com>
2018-11-19 15:12:30 +0800

    .

--- chatbot_demo/chatbot_demo/settings.py
+++ chatbot_demo/chatbot_demo/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'nu2+5s+ih2cw1k^&e5t3wlvvj12k^hg$x$+y1$0sz2&!&-$179'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '158.132.255.98',
+]
 
 
 # Application definition
