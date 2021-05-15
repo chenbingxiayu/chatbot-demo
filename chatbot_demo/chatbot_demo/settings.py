46484f64ac5605ae7ed90a343a8e54c771623a11
preke <geek00021@gmail.com>
2021-05-15 16:28:16 +0800

    zhiyuan update
    declear photo variables: `polly` and `client` in the begining of JS;
    finished the front end of rating, and integrated into the original end()

--- chatbot_demo/chatbot_demo/settings.py
+++ chatbot_demo/chatbot_demo/settings.py
@@ -123,5 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "./main/static"),
 )
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
