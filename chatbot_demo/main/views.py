d0db04926933ac3adfdc1ea9d03e84b91bf8474c
Ken Lai <19791986+kenlai141@users.noreply.github.com>
2021-05-16 13:59:44 +0800

    add views and APIs for StaffStatus and StudentChatStatus

--- chatbot_demo/main/views.py
+++ chatbot_demo/main/views.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 import os
 import json
 import requests
