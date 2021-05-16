a3d9ae62f5fd5e5d146e06935b85b23480d9aca9
Ken Lai <19791986+kenlai141@users.noreply.github.com>
2021-05-16 13:59:44 +0800

    add views and APIs for StaffStatus and StudentChatStatus

--- chatbot_demo/main/models.py
+++ chatbot_demo/main/models.py
@@ -2,43 +2,41 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
-ROLE = [
-    ('online_triage', 'Online Triage'),
-    ('do', 'DO'),
-    ('counsellor', 'Counsellor'),
-    ('supervisor', 'Supervisor'),
-    ('admin', 'Admin')
-]
-
-STAFF_STATUS = [
-    ('available', 'Available'),
-    ('chatting', 'Chatting'),
-    ('offline', 'Offline')
-]
-
-CHAT_STATUS = [
-    ('waiting', 'Waiting'),
-    ('chatting', 'Chatting'),
-    ('end', 'End')
-]
+from django.utils.translation import gettext_lazy as _
 
 
 class StaffStatus(models.Model):
+    class Role(models.TextChoices):
+        ONLINETRIAGE = 'online_triage', _('Online Triage')
+        DO = 'do', _('DO')
+        COUNSELLOR = 'counsellor', _('Counsellor')
+        SUPERVISOR = 'supervisor', _('Supervisor')
+        ADMIN = 'admin', _('Admin')
+
+    class ChatStatus(models.TextChoices):
+        AVAILABLE = 'available', _('Available'),
+        CHATTING = 'chatting', _('Chatting'),
+        OFFLINE = 'offline', _('Offline')
+
     staff_name = models.CharField(max_length=64)
-    staff_net_id = models.CharField(max_length=64, unique=True)
-    staff_role = models.CharField(max_length=32, choices=ROLE)
-    staff_chat_status = models.CharField(max_length=32, choices=STAFF_STATUS)
+    staff_netid = models.CharField(max_length=64, unique=True)
+    staff_role = models.CharField(max_length=32, choices=Role.choices)
+    staff_chat_status = models.CharField(max_length=32, choices=ChatStatus.choices)
     status_change_time = models.DateTimeField()
 
     def __str__(self):
-        return f"Staff({self.staff_net_id}: {self.staff_role})"
+        return f"Staff({self.staff_netid}: {self.staff_role})"
 
 
 class StudentChatStatus(models.Model):
-    student_netid = models.CharField(max_length=64)
+    class ChatStatus(models.TextChoices):
+        WAITING = 'waiting', _('Waiting'),
+        CHATTING = 'chatting', _('Chatting'),
+        END = 'end', _('End')
+
+    student_netid = models.CharField(max_length=64, unique=True)
     chat_request_time = models.DateTimeField(default=None, null=True)
-    student_chat_status = models.CharField(max_length=32, choices=CHAT_STATUS)
+    student_chat_status = models.CharField(max_length=32, choices=ChatStatus.choices)
     chat_start_time = models.DateTimeField(default=None, null=True)
     chat_end_time = models.DateTimeField(default=None, null=True)
-    assigned_counsellor_id = models.ForeignKey(StaffStatus, null=True, on_delete=models.DO_NOTHING)
+    assigned_counsellor = models.ForeignKey(StaffStatus, null=True, on_delete=models.DO_NOTHING)
--- chatbot_demo/main/views.py
+++ chatbot_demo/main/views.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from django.views.decorators.csrf import csrf_exempt
 import os
 import json
 import requests
+
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from django.db import IntegrityError
+from django.core import serializers
+
+from main.models import StaffStatus, StudentChatStatus
 
 
 def index(request):
@@ -50,3 +56,190 @@ def response_api(request):
     # print response.json()['response']
 
     return HttpResponse(response.json()['response'])
+
+
+@csrf_exempt
+def counsellor(request, offset=0, limit=20, staff_netid='1234569a'):
+    staff = StaffStatus.objects.get(staff_netid=staff_netid)
+    students = StudentChatStatus.objects.order_by('chat_request_time')[offset:offset + limit]
+    now = timezone.now()
+    return render(request, 'main/counsellor.html',
+                  {'staff': staff, 'students': students, 'now': now})
+
+
+@csrf_exempt
+def supervisor(request, offset=0, limit=20, staff_netid='1234581a'):
+    staff = StaffStatus.objects.get(staff_netid=staff_netid)
+    students = StudentChatStatus.objects.order_by('chat_request_time')[offset:offset + limit]
+    now = timezone.now()
+    return render(request, 'main/supervisor.html',
+                  {'staff': staff, 'students': students, 'now': now})
+
+
+@csrf_exempt
+def administrator(request, offset=0, limit=20, staff_netid='1234583a'):
+    staff = StaffStatus.objects.get(staff_netid=staff_netid)
+    students = StudentChatStatus.objects.order_by('chat_request_time')[offset:offset + limit]
+    now = timezone.now()
+    return render(request, 'main/administrator.html',
+                  {'staff': staff, 'students': students, 'now': now})
+
+
+@csrf_exempt
+def staffstatus(request, offset=0, limit=20, staff_netid='1234583a'):
+    staff = StaffStatus.objects.get(staff_netid=staff_netid)
+    staff_list = StaffStatus.objects.all()[offset:offset + limit]
+    now = timezone.now()
+    return render(request, 'main/staffstatus.html',
+                  {'staff': staff, 'staff_list': staff_list, 'now': now})
+
+
+@csrf_exempt
+def getseq(request):
+    res = StudentChatStatus.objects \
+        .filter(student_chat_status=StudentChatStatus.ChatStatus.WAITING) \
+        .order_by('-chat_request_time')
+    serialized_data = serializers.serialize('python', res)
+
+    return JsonResponse(serialized_data, safe=False, status=200)
+
+
+@csrf_exempt
+def addstud(request):
+    if request.POST:
+        now = timezone.now()
+        student_status = StudentChatStatus(
+            student_netid=request.POST.get('student_netid'),
+            chat_request_time=now,
+            student_chat_status=StudentChatStatus.ChatStatus.WAITING,
+        )
+        try:
+            student_status.save()
+        except IntegrityError as e:
+            return JsonResponse({'status': 'fail', 'error': str(e)}, status=400)
+        return JsonResponse({'status': 'success'}, status=201)
+
+
+@csrf_exempt
+def getstud(request):
+    res = StudentChatStatus.objects \
+        .filter(student_chat_status=StudentChatStatus.ChatStatus.WAITING) \
+        .order_by('-chat_request_time') \
+        .first()
+    serialized_data = serializers.serialize('python', [res])
+    return JsonResponse(serialized_data[0], safe=False, status=200)
+
+
+@csrf_exempt
+def deletestud(request):
+    """Remove a student from the waiting queue or change her/his status to “End”
+
+    :param request:
+    :return:
+    """
+    if request.POST:
+        student_netid = request.POST.get('student_netid')
+        student_status = StudentChatStatus.objects \
+            .filter(student_chat_status=StudentChatStatus.ChatStatus.WAITING) \
+            .get(student_netid=student_netid)
+        student_status.student_chat_status = StudentChatStatus.ChatStatus.END
+        student_status.save()
+
+        return JsonResponse({'status': 'success'}, status=200)
+
+
+@csrf_exempt
+def updatestud(request):
+    if request.POST:
+        student_netid = request.POST.get('student_netid')
+        new_status = request.POST.get('new_status')
+        student = StudentChatStatus.objects.get(tudent_netid=student_netid)
+        student.student_chat_status = new_status
+        student.chat_end_time = timezone.now()
+        student.save()
+
+        return JsonResponse({'status': 'success'}, status=200)
+
+
+@csrf_exempt
+def getstafflist(request):
+    res = StaffStatus.objects \
+        .filter(staff_chat_status=StaffStatus.ChatStatus.AVAILABLE) \
+        .values('staff_netid', 'staff_role', 'status_change_time')
+    return JsonResponse(list(res), safe=False, status=200)
+
+
+@csrf_exempt
+def getstaff(request):
+    role = request.GET.get('role')
+    qset = StaffStatus.objects.filter(staff_chat_status=StaffStatus.ChatStatus.AVAILABLE)
+    if role:
+        qset = qset.filter(staff_role=role)
+    res = qset.values('staff_netid').first()
+    return JsonResponse(res, safe=False, status=200)
+
+
+@csrf_exempt
+def updatestaff(request):
+    if request.POST:
+        staff_netid = request.POST.get('staff_netid')
+        new_status = request.POST.get('staff_chat_status')
+        staff = StaffStatus.objects.get(staff_netid=staff_netid)
+        staff.staff_chat_status = new_status
+        staff.status_change_time = timezone.now()
+        staff.save()
+
+        return JsonResponse({'status': 'success'}, status=200)
+
+
+@csrf_exempt
+def addstaff(request):
+    if request.POST:
+        now = timezone.now()
+        staff_status = StaffStatus(
+            staff_name=request.POST.get('staff_name'),
+            staff_netid=request.POST.get('staff_netid'),
+            staff_role=request.POST.get('staff_role'),
+            staff_chat_status=request.POST.get('staff_chat_status'),
+            status_change_time=now
+        )
+        try:
+            staff_status.save()
+        except IntegrityError as e:
+            return JsonResponse({'status': 'fail', 'error': str(e)}, status=400)
+        return JsonResponse({'status': 'success'}, status=201)
+
+
+@csrf_exempt
+def deletestaff(request):
+    if request.POST:
+        staff_netid = request.POST.get('staff_netid')
+        delete = request.POST.get('delete')
+        staff = StaffStatus.objects.get(staff_netid=staff_netid)
+        if delete:
+            staff.delete()
+        else:
+            staff.staff_chat_status = StaffStatus.ChatStatus.OFFLINE
+            staff.save()
+
+    return JsonResponse({'status': 'success'}, status=200)
+
+
+@csrf_exempt
+def assignstaff(request):
+    if request.POST:
+        student_netid = request.POST.get('student_netid')
+        staff_netid = request.POST.get('staff_netid')
+        now = timezone.now()
+        staff = StaffStatus.objects.get(staff_netid=staff_netid)
+        student = StudentChatStatus.objects.get(student_netid=student_netid)
+
+        student.student_chat_status = StudentChatStatus.ChatStatus.CHATTING
+        student.chat_start_time = now
+        student.assigned_counsellor_id = staff
+        staff.staff_chat_status = StaffStatus.ChatStatus.CHATTING
+        staff.status_change_time = now
+        student.save()
+        staff.save()
+
+        return JsonResponse({'status': 'success'}, status=200)
