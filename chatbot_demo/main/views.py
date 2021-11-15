2d19d1c8e0d013fd3e16e1e834062dabe8aabb93
Ken Lai <19791986+kenlai141@users.noreply.github.com>
2021-11-15 22:20:31 +0800

    working hour

--- chatbot_demo/main/views.py
+++ chatbot_demo/main/views.py
@@ -694,8 +694,7 @@ def is_working_day(request, date: str):
         response_json['message'] = msg
         return JsonResponse(response_json, status=404)
 
-    # TODO: revert this after uat testing
-    return JsonResponse({'is_working_day': True}, status=200)
+    return JsonResponse({'is_working_day': calendar_date.is_working_day}, status=200)
 
 
 @login_required
@@ -710,5 +709,4 @@ def is_working_hour(request):
         response_json['message'] = msg
         return JsonResponse(response_json, status=404)
 
-    # TODO: revert this after uat testing
-    return JsonResponse({'is_working_hour': True}, status=200)
+    return JsonResponse({'is_working_hour': res}, status=200)
