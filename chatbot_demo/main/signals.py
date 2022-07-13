801efb0dcd139e364e9cb076412fb4d1f11fe8c5
DexunCHENG <chengdexun@gmail.com>
2022-07-13 18:08:12 +0800

    fix(Admin): Clear related staff when user deleted

--- chatbot_demo/main/signals.py
+++ chatbot_demo/main/signals.py
@@ -1,8 +1,9 @@
 import logging
-
+from django.db.models.signals import post_delete
 from django.dispatch import receiver, Signal
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
+from main.models import (StaffStatus, User)
 
 logger = logging.getLogger('django')
 
@@ -30,3 +31,16 @@ def send_update_signal(sender, **kwargs):
         )
     except Exception as e:
         logger.warning(e)
+
+@receiver(post_delete, sender=User)
+def delete_staff(sender, instance, *args, **kwargs):
+    """ Deletes staff status files on `post_delete` """
+    if instance.netid:
+        logger.info("Delete staff after user deleted")
+        try:
+            staff = StaffStatus.objects.get(staff_netid=instance.netid)
+        except StaffStatus.DoesNotExist:
+            logger.warning("Staff does not exist.")
+            return
+        staff.delete()
+        logger.info("Staff deleted.")
